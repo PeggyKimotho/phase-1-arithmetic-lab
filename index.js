63157322f7d8c8363ae71418deb691e48cd3ec76
Peggy <peggykimotho@gmail.com>
2023-06-13 11:55:32 +0000

    Completed

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 18;
+let num4 = 7;
+let multiply = (num1 * num2);
+
+let random = Math.floor(Math.random(0)*20);
+
+let mod = num3 % num4;
+
+let max = Math.max(1,5,6,20);
